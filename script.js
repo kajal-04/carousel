3d7a8fc6a3b1ad8b88cf8a7a1dc695e673fb6b85
Kajal Kumari <kajal.k@keka.com>
2023-10-24 16:01:18 +0530

    optimed code

--- script.js
+++ script.js
@@ -1,97 +1,72 @@
 let slidePosition = 0;
-const sliders = document.querySelectorAll('.slider-item');
-const imageContainer = document.querySelectorAll('.image-container');
-const sliderContent = document.querySelectorAll('.slider-content');
-const totalSlider = sliders.length;
-// const btnPrev = document.querySelector('#btn-prev');
-// const btnNext = document.querySelector('#btn-next');
+const sliderItem = document.querySelectorAll('.js-slider-item');
+const totalSlider = sliderItem.length;
 
-// btnPrev.addEventListener('click', function () {
-//     PrevSlide();
-// });
-// btnNext.addEventListener('click', function () {
-//     NextSlide();
-// })
+updatePosition = (previousSlidePosition) => {
+    dots.forEach(dot => dot.classList.remove('js-dot-active'));
+    dots[slidePosition].classList.add('js-dot-active');
 
-function updatePosition() {
-    sliders.forEach(slide => {
-        // console.log("slides", slide);
-        // slide.classList.remove('active');
-        // slide.classList.add('hidden');
-    });
-
-    imageContainer.forEach(image => {
-        console.log("image", image); 
-        image.classList.remove('imageFadeIn');
-        image.classList.add('imageFadeOut');
+    const sliderImage = document.querySelectorAll('.js-slider-image');
+    sliderImage.forEach(image => {
+        image.classList.remove('js-image-fade-in');
+        image.classList.add('js-image-fade-out');
     })
-    imageContainer[slidePosition].classList.remove('imageFadeOut');
-    imageContainer[slidePosition].classList.add('imageFadeIn');
+    sliderImage[slidePosition].classList.remove('js-image-fade-out');
+    sliderImage[slidePosition].classList.add('js-image-fade-in');
 
-    sliderContent.forEach(slide => {
-        slide.classList.remove('slideIn');
-        slide.classList.add('slideOut');
-    })
-    sliderContent[slidePosition].classList.remove('slideOut');
-    sliderContent[slidePosition].classList.add('slideIn');
-    // sliders[slidePosition].classList.add('active');
-
-    dots.forEach(dot => {
-        dot.classList.remove('dot-active');
-    });
-
-    dots[slidePosition].classList.add('dot-active');
+    positionSlider();
+    slideAnimation(previousSlidePosition);
 }
 
-// function PrevSlide() {
-//     if (slidePosition == 0) {
-//         slidePosition = totalSlider - 1;
-//     } else {
-//         slidePosition--;
-//     }
-//     updatePosition();
-// }
-// function NextSlide() {
-//     if (slidePosition == totalSlider - 1) {
-//         slidePosition = 0;
-//     } else {
-//         slidePosition++;
-//     }
-//     updatePosition();
-// }
+slideAnimation = (previousSlidePosition) => {
+    sliderItem.forEach((item) => {
+        const classList = item.classList;
+        classList.remove('js-slide-left', 'js-slide-right', 'js-slide-left-to-right', 'js-slide-right-to-left');
+    })
+    
+    if (previousSlidePosition === slidePosition) return;
+    else if(previousSlidePosition > slidePosition) {
+        sliderItem[slidePosition].classList.add('js-slide-left-to-right');
+        sliderItem[previousSlidePosition].classList.add('js-slide-right');
+    } else if(previousSlidePosition < slidePosition) {
+        sliderItem[slidePosition].classList.add('js-slide-right-to-left');
+        sliderItem[previousSlidePosition].classList.add('js-slide-left');
+    }
+}
 
+positionSlider = () => {
+    sliderItem.forEach((item, index) => {
+        if(slidePosition == index) {
+            item.style.transform = "translateX(0%)";
+        } else {
+            item.style.transform = index < slidePosition ? `translateX(-100%)` : `translateX(100%)`;
+        }
+    });
+}
 
-const dotContainer = document.querySelector('.dots');
-sliders.forEach(slide => {
-    const dot = document.createElement('div');
-    dot.classList.add('dot');
+const dotContainer = document.querySelector('.js-slider-indicator');
+const imageContainer = document.querySelector('.js-image-container');
+
+sliderItem.forEach((slide, index) => {
+    const dot = document.createElement('li');
+    dot.classList.add('js-indicator-dot');
     dotContainer.appendChild(dot);
+
+    const img = document.createElement('img');
+    img.classList.add('js-slider-image');
+    img.src=`assets/carousel-${index}.png`;
+    imageContainer.appendChild(img);
 });
 
-const dots = document.querySelectorAll('.dot');
-dots[slidePosition].classList.add('dot-active');
+const dots = document.querySelectorAll('.js-indicator-dot');
+dots[slidePosition].classList.add('js-dot-active');
 
 dots.forEach((dot, index) => {
     dot.addEventListener('click', function () {
+        let previousSlidePosition = slidePosition;
         slidePosition = index;
-        updatePosition();
+        updatePosition(previousSlidePosition);
     });
 });
 
-imageContainer.forEach((image, index) => {
-    const img = document.createElement('img');
-    img.classList.add('slider-image');
-    img.src=`assets/carousel-${index}.png`;
-    image.appendChild(img);
-});
-
-        updatePosition();
-
-// setInterval(()=>{
-//   if(slidePosition==totalSlider-1){
-//     slidePosition=0;
-//   }else{
-//     slidePosition++;
-//   }
-//   updatePosition();
-// },5000);
+updatePosition();
